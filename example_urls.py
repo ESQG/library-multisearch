38a8de6312f38763ce41a04ac7f5fc65746be4ff
ESQG <esqg@alumni.stanford.edu>
2017-02-06 15:07:45 -0800

    Beautiful Soup for HTML processing

---
+++ example_urls.py
@@ -0,0 +1,19 @@
+catalog_entry = ["https://sfpl.bibliocommons.com/item/show/1024641093_the_handmaids_tale",
+"https://sfpl.bibliocommons.com/item/show/2294407093_fledgling",
+"https://sfpl.bibliocommons.com/item/show/2294407093",
+"https://sfpl.bibliocommons.com/item/show/1970540093_fledgling",
+"https://sfpl.bibliocommons.com/item/show/3018942093_fledgling",
+]
+
+
+availability = ["https://sfpl.bibliocommons.com/item/show_circulation/1024641093_the_handmaids_tale", 
+"https://sfpl.bibliocommons.com/item/show_circulation/1024641093",
+"https://sfpl.bibliocommons.com/item/show_circulation/1970540093",
+"https://sfpl.bibliocommons.com/item/show_circulation/2294407093",
+"https://sfpl.bibliocommons.com/item/show_circulation/3018942093_fledgling"
+]
+
+
+search_query = ["https://sfpl.bibliocommons.com/search?custom_query=(contributor%3A(Octavia+Butler)+AND+title%3A(Fledgling))",
+]
+
