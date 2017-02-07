ce04a00a81470702e92072c14b10325880b47c44
ESQG <esqg@alumni.stanford.edu>
2017-02-06 16:12:27 -0800

    working on parsing HTML

--- example_urls.py
+++ example_urls.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 catalog_entry = ["https://sfpl.bibliocommons.com/item/show/1024641093_the_handmaids_tale",
 "https://sfpl.bibliocommons.com/item/show/2294407093_fledgling",
 "https://sfpl.bibliocommons.com/item/show/2294407093",
@@ -17,3 +20,11 @@ availability = ["https://sfpl.bibliocommons.com/item/show_circulation/1024641093
 search_query = ["https://sfpl.bibliocommons.com/search?custom_query=(contributor%3A(Octavia+Butler)+AND+title%3A(Fledgling))",
 ]
 
+def soupify(link):
+    response = requests.get(link)
+    if response.status_code == 200:
+        raw_html = response.content
+        return BeautifulSoup(raw_html, "html.parser")
+    else:
+        print "Cannot find %s" % link   # May want to make an error?
+        return None
