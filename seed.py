77ff278e2708f3eab7cfd03834f7b63f1936db60
esqg <esqg@alumni.stanford.edu>
2017-02-10 01:55:12 +0000

    added goodreads account and began drafting more fillers

--- seed.py
+++ seed.py
@@ -7,10 +7,18 @@ import data_manager
 
 connect_to_db(app)
 
+def sfpl_branches():
+    sfpl = LibrarySystem(library_code="sfpl", name="San Francisco Public Library")
+    db.session.add(sfpl)
+    pass
+
+
+
 def add_my_toread_list():
     esqg = User(first_name="Elizabeth", last_name="Goodman", email="esqg@nowhere.com", password="programmer")
     db.session.add(esqg)
     db.session.commit()
+    goodreads = GoodreadsUser(user_id=esqg.user_id, goodreads_id="2416346")
 
     with open('stuff2.xml', 'r') as esqg_toread_file:
         book_list_soup = BeautifulSoup(esqg_toread_file.read(), "html.parser")
@@ -21,6 +29,7 @@ def add_my_toread_list():
         book = Book(title=book_data['title'], author=book_data['author'])
         db.session.add(book)
         db.session.commit()
+
 
         assoc = UserBook(book_id = book.book_id, user_id = esqg.user_id)
         db.session.add(assoc)
