b3c9755747368c0f12dfffa28c73ed621cd6823d
ESQG <esqg@alumni.stanford.edu>
2017-02-13 17:22:40 -0800

    Connecting database to API searches: get records for a book, update availability

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,11 @@
+from server import app
+from model import *
+import use_sfpl
+import sfpl_locations
+import re
+from datetime import datetime
+
+
 def log_overlaps(title, author):
 
     search_author = author.split().strip(',')
@@ -9,7 +17,7 @@ def log_overlaps(title, author):
     overlaps = Book.query.filter(Book.title.like('%' + search_title + '%'), Book.author.like('%' + search_author + '%'))
 
     if overlaps:
-        with open('overlap-log.txt', 'a') as outfile:
+        with open('overlaps.log', 'a') as outfile:
             outfile.write(datetime.now().isoformat())
             outfile.write('\n')
             outfile.write("Found overlaps: ")
@@ -19,6 +27,7 @@ def log_overlaps(title, author):
                 outfile.write('\t')
                 outfile.write(str(book))
                 outfile.write('\n')
+
 
 def add_book(title, author):
     already_there = Book.query.filter_by(title=title, author=author).one()
@@ -32,3 +41,131 @@ def add_book(title, author):
     db.session.add(book)
     db.session.commit()
     return book
+
+
+def records_from_book(book):
+    
+    record_data = use_sfpl.search_all_records(book.title, book.author)
+    desired_records = []
+
+    for data in record_data:
+        if check_matching_title(book, data['title']) and check_matching_author(book, data['author']):
+
+            bibliocommons_id_finder = re.search(r'\d{9,11}', data['path'])
+            if bibliocommons_id_finder:
+                bibliocommons_id = bibliocommons_id_finder.group()
+            else:
+                write_log("No id found in path", str(data)) #Error will be raised by SQLAlchemy
+
+        format_code = codify_format(data['format'])
+
+        if format_code and bibliocommons_id:
+            old_record = Record.query.filter_by(bibliocommons_id=bibliocommons_id, book_id=book.book_id).first()
+            if not old_record:
+                record = Record(book_id=book.book_id, bibliocommons_id=bibliocommons_id, format_code=format_code)
+                db.session.add(record)
+                db.session.commit()
+            else:
+                record = old_record
+
+            desired_records.append(record)
+
+        else:
+            write_log("Bad search result", str(book), str(data))
+
+    return desired_records
+
+def update_availability(record):
+
+    if record.format.digital:
+        return
+
+    current_availability = sfpl_locations.plain_results(record)
+    current_availability.sort(key=lambda x: (x['status'] == 'CHECK SHELF'))
+        # Put all CHECK SHELF statuses last
+
+    tracking_branches = {}
+
+    for result in current_availability:
+        if result['branch'] in tracking_branches:
+            branch, association = tracking_branches[result['branch']]
+
+            association.total_copies += 1
+            db.session.commit()
+
+        else:
+            branch = Branch.query.filter_by(name=result['branch']).one()
+            association = RecordBranch.query.filter_by(branch_code=branch.branch_code, record_id=record.record_id).first()
+            if not association:
+                association = RecordBranch(branch_code=branch.branch_code, record_id=record.record_id)
+                db.session.add(association)
+                db.session.commit()
+
+            tracking_branches[branch.name] = [branch, association]
+            association.total_copies = 1
+
+        shelf = CallNumber.query.filter_by(record_id=association.recbranch_id, call_number=result['call_number']).first()
+        if shelf:
+            if shelf.total_available > 0 and result['status'] != 'CHECK SHELF':
+                shelf.total_available = 0
+            elif result['status'] == 'CHECK SHELF':
+                shelf.total_available = 1   # Adjust later to library grouping
+        else:
+            shelf = CallNumber(record_id=association.recbranch_id, call_number=result['call_number'])
+            db.session.add(shelf)
+            if result['status'] == 'CHECK SHELF':
+                shelf.total_available = 1
+            else:
+                shelf.total_available = 0   # Happens first, overwritten by available ones
+        db.session.commit()
+
+    return current_availability  # For convenience in interactive mode
+
+
+def check_matching_title(book, title):
+    """Check if a title is correct.  Need to be careful about this!"""
+
+    return True
+
+
+def check_matching_author(book, author):
+    """Check if an author is correct.  Need to be careful about this!"""
+
+    return True
+
+
+def codify_format(format_string):
+    """Returns None if format is non-standard."""
+
+    format_string = format_string.strip('()').strip()
+    if format_string == 'eBook':
+        return 'ebook'
+    elif format_string == 'Book':
+        return 'book'
+    elif 'Audiobook' in format_string:
+        if 'CD' in format_string:
+            return 'audiocd'
+        elif 'Downloadable' in format_string:
+            return 'eaudio'
+        elif 'hoopla' in format_string.lower():
+            return 'hoopla'
+    elif 'DVD' in format_string:
+        pass
+    else:
+        write_log("New format found", format_string)
+
+
+
+def write_log(message, *args):
+    with open("data_manager.log", 'a') as log_file:
+        log_file.write('\n')
+        log_file.write(datetime.now().isoformat())
+        log_file.write('\t')
+        log_file.write(message + ":")
+        log_file.write('\t')
+        log_file.write('\n'.join(args))
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+
