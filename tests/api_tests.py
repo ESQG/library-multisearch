d3fe60e09002fed882e968ce9f2b1973a47d0bac
esqg <esqg@alumni.stanford.edu>
2017-02-22 23:18:19 +0000

    added availability page for mocks; will come back to this

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -9,11 +9,15 @@ SHORT_SHELF = "https://www.goodreads.com/review/list/2416346?shelf=maybe-someday
 ESQG="2416346"
 
 class QueryTests(unittest.TestCase):
+    
     def test_get_shelf(self):
+
         data = use_goodreads.parse_shelf_and_id(SHORT_SHELF)
         shelf = data['shelf']
         goodreads_id = data['goodreads_id']
+
         results = use_goodreads.get_shelf(goodreads_id, shelf)
+
         self.assertIn('Asimov', str(results))
         self.assertEqual(type(results), list)
         self.assertGreater(len(results), 0)
