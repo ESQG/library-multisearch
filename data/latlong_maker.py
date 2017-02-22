9fa0ba318ecc55ce69d283a071c02faa8af70dde
esqg <esqg@alumni.stanford.edu>
2017-02-22 23:40:42 +0000

    decided to protect one-time code under ifmain

--- data/latlong_maker.py
+++ data/latlong_maker.py
@@ -15,18 +15,23 @@ from server import app
 # Setup: database connection, api key, branches, 
 GEOCODE_KEY = os.environ['GOOGLE_GEOCODE_API_KEY']
 API_URL = "https://maps.googleapis.com/maps/api/geocode/json"
-connect_to_db(app)
 
-library_branches = Branch.query.all()
-geocode_data = []
 
-for branch in library_branches:
-    response = requests.get(API_URL, {'address': branch.address, 'key': GEOCODE_KEY})
-    if response.status_code != 200:
-        print response.url
-    else:
-        data = response.json()
-        latlong = data['results'][0]['geometry']['location']
-        branch.latitude = latlong['lat']
-        branch.longitude = latlong['lng']
-db.session.commit()
+def main():
+    connect_to_db(app)
+
+    library_branches = Branch.query.all()
+
+    for branch in library_branches:
+        response = requests.get(API_URL, {'address': branch.address, 'key': GEOCODE_KEY})
+        if response.status_code != 200:
+            print response.url
+        else:
+            data = response.json()
+            latlong = data['results'][0]['geometry']['location']
+            branch.latitude = latlong['lat']
+            branch.longitude = latlong['lng']
+    db.session.commit()
+
+if __name__ == '__main__':
+    main()
