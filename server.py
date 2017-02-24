dd37d1fcc43b89b7eb7af374b16ad85311b6f450
esqg <esqg@alumni.stanford.edu>
2017-02-24 01:06:22 +0000

    Back end calibrated for Googlemap

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, session, flash, redirect, request, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from datetime import datetime
+import os
+
 from data.seed import SFPL_BRANCHES
 from data import data_manager
 from queries import use_goodreads
 
+GOOGLEMAPS_KEY = os.environ["GOOGLEMAPS_API_KEY"]
 app = Flask(__name__)
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
 
@@ -70,11 +73,22 @@ def display_books_from_session():
 @app.route("/books.json")
 def show_branches_and_bookids():
     branches = data_manager.branch_dict_list("sfpl")
+    avg_lat = 37.75774
+    avg_long = -122.43870
+    bounds = {'lat': [-122.54, -122.35], 'lng': [37.67, 37.84]}
+    center = {'lat': avg_lat, 'lng': avg_long}
+
+
+    data_to_serve = {'branches': branches,
+                     'map_center': center,
+                     'map_bounds': bounds,
+                     }
 
     if 'books' in session:
-        return jsonify({'book_ids': session['books'], 'branches': branches})
+        data_to_serve['book_ids'] = session['books']
     else:
-        return jsonify({'book_ids': [], 'branches': branches})
+        data_to_serve['book_ids'] = []
+    return jsonify(data_to_serve)
 
 @app.route("/book/<book_id>.json")
 def book_info(book_id):
@@ -103,7 +117,7 @@ def book_info(book_id):
 def library_books_page():
 
     codes_and_names = sorted(SFPL_BRANCHES.items(), key=lambda tup: tup[1])
-    return render_template("library_books.html", codes_and_names=codes_and_names)
+    return render_template("library_books.html", codes_and_names=codes_and_names, map_key=GOOGLEMAPS_KEY)
 
 
 def write_log(*args):
